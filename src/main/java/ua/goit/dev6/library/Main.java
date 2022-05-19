92eefebb09507b20a6884c210f4dd64664f27544
sergeyonischuk <sergey.onischuk@ukr.net>
2022-05-19 20:09:36 +0300

    service class Library created. Excercise 5

--- src/main/java/ua/goit/dev6/library/Main.java
+++ src/main/java/ua/goit/dev6/library/Main.java
@@ -2,12 +2,19 @@ package ua.goit.dev6.library;
 
 public class Main {
     public static void main(String[] args) {
-        Publication publication = new Publication("Book",60);
-        System.out.println(publication.print());
-        Book book = new Book("Book", 127, "anonim");
-        System.out.println(book.print());
-        Journal journal = new Journal("Journal", 55, 7, 2022);
-        System.out.println(journal.print());
+//        Publication publication = new Publication("Book",60);
+//        System.out.println(publication.print());
+//        Book book = new Book("Book", 127, "anonim");
+//        System.out.println(book.print());
+//        Journal journal = new Journal("Journal", 55, 7, 2022);
+//        System.out.println(journal.print());
+        Library library = new Library();
+        library.printPublications(null);
+        library.printPublications(new Publication[0]);
+        library.printPublications(new Publication[]{new Journal("PC", 100, 1, 2021)});
+        library.printPublications(new Publication[]{new Book("Witcher", 200, "Andrzej Sapkowski")});
+        library.printPublications(new Publication[]{new Book("Master & Margarita", 300, "Mikhail Bulgakov"),
+                new Journal("Gamer", 101, 10, 2018)});
     }
 
 }
