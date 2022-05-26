22832acbea870957ede5982fd00d058efd68311f
sergeyonischuk <sergey.onischuk@ukr.net>
2022-05-26 21:13:29 +0300

    add BookService

---
+++ src/main/java/ua/goit/dev6/library/BookService.java
@@ -0,0 +1,31 @@
+package ua.goit.dev6.library;
+
+import java.util.Scanner;
+
+public class BookService {
+    private final Repository repository;
+
+    public BookService(Repository repository) {
+        this.repository = repository;
+    }
+
+    public void addBookToRepository(Scanner scanner) {
+        System.out.println("enter book name: ");
+        String name = scanner.nextLine();
+        int pageCount = -1;
+        while (true) {
+            try {
+                System.out.println("enter count of pages: ");
+                pageCount = Integer.parseInt(scanner.nextLine());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("invalid value. Use digits");
+            }
+        }
+        System.out.println("enter author of the book: ");
+        String author = scanner.nextLine();
+        Book book = new Book(name, pageCount, author);
+        repository.add(book);
+        System.out.println("book added. Thank you!");
+    }
+}
--- src/main/java/ua/goit/dev6/library/Main.java
+++ src/main/java/ua/goit/dev6/library/Main.java
@@ -5,8 +5,11 @@ import java.util.Scanner;
 public class Main {
     private static final String HELP_COMMAND = "help";
     private static final String EXIT_COMMAND = "exit";
+    private static final String ADD_BOOK_COMMAND = "add book";
 
     public static void main(String[] args) {
+        Repository repository = new Repository();
+        BookService bookService = new BookService(repository);
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hello, please enter help to see all command");
         while (true) {
@@ -17,10 +20,12 @@ public class Main {
             } else if (command.equals(EXIT_COMMAND)) {
                 System.out.println("Good bye");
                 return;
-            } else {
+            } else if (command.equals(ADD_BOOK_COMMAND)) {
+                bookService.addBookToRepository(scanner);
+            }
+            else {
                 System.out.println("Command not found");
             }
         }
-
     }
 }
