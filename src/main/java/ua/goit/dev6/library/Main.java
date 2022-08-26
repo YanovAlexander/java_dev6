c21f8dd1a2e1a99c2410aa257ce09d97c0fe9695
alexyanov <yanov.alexander@gmail.com>
2022-08-26 20:15:47 +0300

    Add database manager

--- src/main/java/ua/goit/dev6/library/Main.java
+++ src/main/java/ua/goit/dev6/library/Main.java
@@ -4,6 +4,8 @@ import ua.goit.dev6.library.command.AddBook;
 import ua.goit.dev6.library.command.Command;
 import ua.goit.dev6.library.command.Exit;
 import ua.goit.dev6.library.command.Help;
+import ua.goit.dev6.library.config.DatabaseManagerConnector;
+import ua.goit.dev6.library.config.PropertiesConfig;
 import ua.goit.dev6.library.controller.Library;
 import ua.goit.dev6.library.repository.InMemoryArrayRepository;
 import ua.goit.dev6.library.repository.Repository;
@@ -11,12 +13,21 @@ import ua.goit.dev6.library.service.JournalService;
 import ua.goit.dev6.library.view.Console;
 import ua.goit.dev6.library.view.View;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
+        String dbPassword = System.getenv("dbPassword");
+        String dbUsername = System.getenv("dbusername");
+        PropertiesConfig propertiesConfig = new PropertiesConfig();
+        Properties properties = propertiesConfig.loadProperties("application.properties");
+
+        DatabaseManagerConnector connector = new DatabaseManagerConnector(properties, dbUsername, dbPassword);
+        connector.getConnection();
         Repository repository = new InMemoryArrayRepository();
         JournalService journalService = new JournalService(repository);
         Scanner scanner = new Scanner(System.in);
