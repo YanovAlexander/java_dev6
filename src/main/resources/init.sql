a50d5d363173275322b377f24e09d47c704eb5a8
voicehovski <help.kiev@gmail.com>
2022-09-01 21:42:58 +0300

    Create author to book sql relation

--- src/main/resources/init.sql
+++ src/main/resources/init.sql
@@ -1,11 +1,11 @@
-CREATE TABLE author(
+CREATE TABLE IF NOT EXISTS author(
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(1000),
     last_name VARCHAR(1000),
     email VARCHAR(1000)
 );
 
-CREATE TABLE BOOK(
+CREATE TABLE IF NOT EXISTS BOOK(
     id SERIAL PRIMARY KEY,
     name VARCHAR(1000),
     count_pages INT,
@@ -16,3 +16,17 @@ CREATE TABLE BOOK(
 
 ALTER TABLE AUTHOR
 ADD CONSTRAINT EMAIL_UNIQUE UNIQUE (EMAIL);
+
+CREATE TABLE IF NOT EXISTS author_book_relation(
+    id SERIAL PRIMARY KEY,
+    author_id INT REFERENCES author(id),
+    book_id INT REFERENCES BOOK(id),
+    UNIQUE(author_id, book_id)
+)
+
+INSERT INTO author_book_relation
+    (book_id,author_id)
+(SELECT id,author_id FROM BOOK);
+
+ALTER TABLE BOOK
+	DROP COLUMN author_id;
